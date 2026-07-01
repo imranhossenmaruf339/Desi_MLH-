78c8da7080c32923fd56dac7581978efecef6b2c
imranhossenmaruf339 <imranhossenmaruf339@gmail.com>
2026-07-01 17:37:47 +0200

    Add configuration for environment variables

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+OWNER_ID = int(os.getenv("OWNER_ID"))
+MONGO_URI = os.getenv("MONGO_URI")
