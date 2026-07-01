2e292cf5a12962030c7f8f6d6657617a17050187
imranhossenmaruf339 <imranhossenmaruf339@gmail.com>
2026-07-01 17:15:40 +0200

    Add help command to bot

---
+++ help.py
@@ -0,0 +1,12 @@
+from pyrogram import Client, filters
+
+@Client.on_message(filters.command("help"))
+async def help_cmd(client, message):
+    await message.reply_text(
+        "📌 Commands:\n"
+        "/start - Start bot\n"
+        "/help - Help menu\n"
+        "/profile - Your profile\n"
+        "/daily - Claim reward\n"
+        "/referral - Referral link"
+    )
